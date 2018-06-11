35eebd41c2b2417ab66bf437e380ac3bf72ade74
xiaoyumu <xiaoyumu@gmail.com>
2018-06-11 21:56:40 +0800

    Refactor paraparser to have ability to validate parameters

--- commandline/paraparser.go
+++ commandline/paraparser.go
@@ -1,6 +1,7 @@
 package commandline
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -18,22 +19,64 @@ const minimumDelimiterIndex = 2
 // ParameterPool contains the command line parameters
 type ParameterPool struct {
 	parameters map[string]string
+	setting    *ParameterParseSetting
+}
+
+// ParameterParseSetting contains setting that used
+// while parsing command line parameters
+type ParameterParseSetting struct {
+	prefix                   string
+	delimiter                string
+	requiredParameters       []string
+	actionOnValidationFailed func() // When parameter validation failed, the action will be executed.
 }
 
 // CreateDefault the ParameterPool instance and return the pointer
 // with default parameter prefix and delimiter
-func CreateDefault() *ParameterPool {
-	return Create(DefaultParameterPrefix, DefaultNameValueDelimiter)
+func CreateDefault() (*ParameterPool, error) {
+	return Create(&ParameterParseSetting{
+		prefix:    DefaultParameterPrefix,
+		delimiter: DefaultNameValueDelimiter,
+	})
 }
 
 // Create the ParameterPool instance and return the pointer with
-// given parameter prefix and delimiter
-func Create(prefix string, delimiter string) *ParameterPool {
+// given parameter parse setting
+func Create(s *ParameterParseSetting) (*ParameterPool, error) {
+	if s == nil {
+		panic("parameter s cannot be nil while creating a ParameterPool")
+	}
 	pool := ParameterPool{
-		parameters: getParameters(prefix, delimiter),
+		parameters: getParameters(s.prefix, s.delimiter),
+		setting:    s,
 	}
 
-	return &pool
+	err := pool.validate()
+	if err != nil {
+		if s.actionOnValidationFailed != nil {
+			s.actionOnValidationFailed()
+		}
+
+		return nil, err
+	}
+
+	return &pool, nil
+}
+
+func (p *ParameterPool) validate() error {
+	// If no required parameter specified
+	// just skip the validation
+	if len(p.setting.requiredParameters) <= 0 {
+		return nil
+	}
+
+	for _, requiredParameter := range p.setting.requiredParameters {
+		if !p.HasParameter(requiredParameter) {
+			return fmt.Errorf("the parameter %s is required, but not present", requiredParameter)
+		}
+	}
+
+	return nil
 }
 
 // The first arg of os.Args is the program itself.
@@ -108,4 +151,15 @@ func (p *ParameterPool) Iterate(action func(string, string)) {
 // Count how mange parameters in the pool
 func (p *ParameterPool) Count() int {
 	return len(p.parameters)
+}
+
+// HasParameter checks if a given parameter exists in the pool
+func (p *ParameterPool) HasParameter(name string) bool {
+	for parameterName := range p.parameters {
+		if parameterName == name {
+			return true
+		}
+	}
+
+	return false
 }
