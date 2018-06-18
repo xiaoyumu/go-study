2d15d96e8e5c1aed1bf3e99eff90e3552bb4883e
xiaoyumu <xiaoyumu@gmail.com>
2018-06-18 22:56:47 +0800

    etcd watch demo

--- etcd/etcd-client-demo.go
+++ etcd/etcd-client-demo.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
@@ -20,6 +21,19 @@ func main() {
 		log.Fatal(err)
 	}
 	kapi := client.NewKeysAPI(c)
+
+	watcherOptions := client.WatcherOptions{Recursive: true}
+	watcher := kapi.Watcher("/foo", &watcherOptions)
+
+	go onChanged(&watcher)
+
+	fmt.Print("Press 'Enter' to exit...")
+	for {
+		time.Sleep(200)
+	}
+}
+
+func interactWithEtcd(kapi client.KeysAPI) {
 	// set "/foo" key with "bar" value
 	log.Print("Setting '/foo' key with 'bar' value")
 	resp, err := kapi.Set(context.Background(), "/foo", "bar", nil)
@@ -39,5 +53,36 @@ func main() {
 		log.Printf("Get is done. Metadata is %q\n", resp)
 		// print value
 		log.Printf("%q key has %q value\n", resp.Node.Key, resp.Node.Value)
+	}
+}
+
+func onChanged(watcher *client.Watcher) {
+	for {
+		resp, err := (*watcher).Next(context.Background())
+		if err != nil {
+			log.Println("Failed to get next event on watcher : ", err)
+			break
+		}
+
+		log.Println(fmt.Sprintf("Action: [%s] on Node [%s] (Value: %s).", resp.Action, resp.Node.Key, resp.Node.Value))
+		if resp.Action == "expire" {
+			log.Println(fmt.Sprintf("Node [%s] (Value: %s) has expired.", resp.Node.Key, resp.Node.Value))
+			continue
+		}
+
+		if resp.Action == "set" {
+			log.Println(fmt.Sprintf("Set node [%s] (Value: %s).", resp.Node.Key, resp.Node.Value))
+			continue
+		}
+
+		if resp.Action == "update" {
+			log.Println(fmt.Sprintf("Node [%s] updated, new value: %s", resp.Node.Key, resp.Node.Value))
+			continue
+		}
+
+		if resp.Action == "delete" {
+			log.Println(fmt.Sprintf("Node [%s] (Value: %s) has been deleted.", resp.Node.Key, resp.Node.Value))
+			continue
+		}
 	}
 }
