ed38650e9cfff01a3706bec8005812f971dc3f51
xiaoyumu <xiaoyumu@gmail.com>
2018-07-12 23:05:04 +0800

    Add client implementation

---
+++ grpc/client/remoteDBClient.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	rda "github.com/xiaoyumu/go-study/grpc/proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := rda.NewRemoteDBServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	response, err := c.Execute(ctx, &rda.DbRequest{
+		Server:   "DBServer",
+		Port:     1433,
+		UserId:   "dev",
+		Password: "dev2dev",
+	})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Remote Result is : %s", response.Result)
+}
--- grpc/server/remoteDBServer.go
+++ grpc/server/remoteDBServer.go
@@ -1,21 +1,41 @@
 package main
 
-/*
 import (
 	"log"
 	"net"
+	"os"
 
+	rda "github.com/xiaoyumu/go-study/grpc/proto"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	rda "github.com/xiaoyumu/go-study/grpc/proto/remoteDataAccess"
 	"google.golang.org/grpc/reflection"
 )
-*/
 
 const (
 	port = ":50051"
 )
 
-func main() {
+type server struct{}
 
+func (s *server) Execute(context.Context, *rda.DbRequest) (*rda.DbResponse, error) {
+	log.Println("RPC call received.")
+	return &rda.DbResponse{
+		Result: "Succeeded",
+	}, nil
+}
+
+func main() {
+	log.Printf("Starting RPC server @ Port %v ...", port)
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	rda.RegisterRemoteDBServiceServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+		os.Exit(-1)
+	}
 }
