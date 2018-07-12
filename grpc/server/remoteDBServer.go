35e3ebccfa18540c513d884ac235aa55c017b240
xiaoyumu <xiaoyumu@gmail.com>
2018-07-12 22:35:17 +0800

    Rearrange proto files add server and client folder

---
+++ grpc/server/remoteDBServer.go
@@ -0,0 +1,21 @@
+package main
+
+/*
+import (
+	"log"
+	"net"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	rda "github.com/xiaoyumu/go-study/grpc/proto/remoteDataAccess"
+	"google.golang.org/grpc/reflection"
+)
+*/
+
+const (
+	port = ":50051"
+)
+
+func main() {
+
+}
