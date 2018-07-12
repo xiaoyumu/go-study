2c4e5e63ed147458674a21aa48b19765b062534c
xiaoyumu <xiaoyumu@gmail.com>
2018-07-12 23:37:03 +0800

    Update server code

--- grpc/server/remoteDBServer.go
+++ grpc/server/remoteDBServer.go
@@ -17,24 +17,37 @@ const (
 
 type server struct{}
 
-func (s *server) Execute(context.Context, *rda.DbRequest) (*rda.DbResponse, error) {
+func (s *server) Execute(ctx context.Context, req *rda.DbRequest) (*rda.DbResponse, error) {
 	log.Println("RPC call received.")
+	dumpRemoteDbRequest(req)
+
 	return &rda.DbResponse{
 		Result: "Succeeded",
 	}, nil
 }
 
+func dumpRemoteDbRequest(req *rda.DbRequest) {
+	log.Printf("Dumping request : Server=%v:%v;UID=%v;PWD=%v ...",
+		req.Server,
+		req.Port,
+		req.UserId,
+		req.Password)
+	log.Printf("SQL:%s", req.SqlStatement)
+}
+
 func main() {
 	log.Printf("Starting RPC server @ Port %v ...", port)
-	lis, err := net.Listen("tcp", port)
+	tcpPort, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
-	rda.RegisterRemoteDBServiceServer(s, &server{})
+	grpcSvr := grpc.NewServer()
+
+	// The server struct implemented the RemoteDBServiceServer interface
+	rda.RegisterRemoteDBServiceServer(grpcSvr, &server{})
 	// Register reflection service on gRPC server.
-	reflection.Register(s)
-	if err := s.Serve(lis); err != nil {
+	reflection.Register(grpcSvr)
+	if err := grpcSvr.Serve(tcpPort); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 		os.Exit(-1)
 	}
